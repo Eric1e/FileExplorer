bb13a63e87aa9b8cb4e99a5b09d2765b60318ddc
eric <esheen11@gmail.cpom>
2018-12-17 14:07:58 -0500

    Add interface to do multiple selection

--- src/Model/Selector.java
+++ src/Model/Selector.java
@@ -1,9 +1,12 @@
 package Model;
 
 import java.io.File;
+import java.util.List;
 
 public interface Selector {
 
     public File select(String string);
+
+    public List<File> multiSelect();
 
 }
--- src/Model/SelectorImpl.java
+++ src/Model/SelectorImpl.java
@@ -3,12 +3,20 @@ package Model;
 import Model.Selector;
 
 import java.io.File;
+import java.util.List;
 
 public class SelectorImpl implements Selector {
 
     @Override
     public File select(String string) {
+
         return new File(string);
+    }
+
+    @Override
+    public List<File> multiSelect() {
+
+        return null;
     }
 
 }
